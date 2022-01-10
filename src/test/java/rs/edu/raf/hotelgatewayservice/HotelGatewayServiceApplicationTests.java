633c60e6ff78fe1ede0b472bfbf238b65bb7a332
DimitrijeSpasojevic <dspasojevic134@gmail.com>
2022-01-10 17:10:38 +0100

    Initial commit

---
+++ src/test/java/rs/edu/raf/hotelgatewayservice/HotelGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.edu.raf.hotelgatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
